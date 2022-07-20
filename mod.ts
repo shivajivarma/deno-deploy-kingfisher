f24d8929beaf0c7582abe79faaebfa26c5b6315f
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2022-07-20 18:24:30 +0530

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,3 @@
+import CommonUtils from 'https://raw.githubusercontent.com/justaos/utils/1.1.0/common-utils/mod.ts';
+const temp = CommonUtils.underscoreToCamelCase('hello_world');
+console.log(temp);
