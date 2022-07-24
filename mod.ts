ad6ec7c2b637aa1d939a1c6e3f4b4a41263d7de9
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2022-07-24 11:11:57 +0530

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { existsSync, expandGlobSync, copy, copySync } from 'https://deno.land/std@0.95.0/fs/mod.ts';
+import { expandGlobSync } from 'https://deno.land/std@0.95.0/fs/mod.ts';
 
 for (const file of expandGlobSync('**')) {
   console.log("PATH" + file.path);
