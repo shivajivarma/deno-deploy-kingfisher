7286adcb9d1bcc9a5b9185f23e4b6cb1125ab7d4
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2022-07-24 11:32:29 +0530

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@ import * as path from "https://deno.land/std@0.57.0/path/mod.ts";
 
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
-console.log(__dirname);
+console.log("SHYIVAJI :: " + __dirname);
 
 for (const file of expandGlobSync(__dirname+ '/**')) {
   console.log("PATH" + file.path);
